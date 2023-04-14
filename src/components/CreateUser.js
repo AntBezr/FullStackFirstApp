0c0d0ca81191a2058b795cdcc3cfd8734655e0d2
AntBezr <sosleepyanton@gmail.com>
2023-04-14 11:02:56 +0300

    first commit

---
+++ src/components/CreateUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+const CreateUser = () => {
+  return (
+    <div>
+      <h1>Create users</h1>;
+    </div>
+  );
+};
+
+export default CreateUser;
