08fcf4eff9724a6a382494065224167d988173d7
AntBezr <sosleepyanton@gmail.com>
2023-04-14 11:08:34 +0300

    Created appjs and two links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      full stack app
+     <h5> full stack app</h5>
+     <BrowserRouter>
+     <nav>
+      <ul>
+        <li>
+          <Link to="/">List Users</Link>
+        </li>
+        <li>
+          <Link to="user/create">Create Users</Link>
+        </li>
+      </ul>
+     </nav>
+     </BrowserRouter>
+
     </div>
   );
 }
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 
 const CreateUser = () => {
   return (
